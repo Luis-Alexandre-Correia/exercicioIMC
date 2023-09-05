c63df953b6257073fcb02305f56bd6e5ae993ba3
Luis <luisalexandrepc@gmail.com>
2023-09-05 16:56:07 -0300

    putz

--- secao2/Aula 2 - datas/data.js
+++ secao2/Aula 2 - datas/data.js
@@ -10,10 +10,13 @@ const data3 = new Date('2023-09-05 15:46:59');//a data pode ser digitada em stri
 
 console.log(data3);
 console.log('Dia', data3.getDate()); //getDate extrai apenas o dia da data
-console.log('Mês', data3.getMonth());
-console.log('Ano', data3.getFullYear());
-console.log('Hora', data3.getHours());
-console.log('Minuto', data3.getMinutes());
-console.log('Segundo', data3.getSeconds());
-console.log('Milisegundos', data3.getMilliseconds())
-console.log('Dia da semana', data3.getDay());
+console.log('Mês', data3.getMonth()); //Extrai o mes
+console.log('Ano', data3.getFullYear()); //extrai o ano
+console.log('Hora', data3.getHours());// extrai a hora
+console.log('Minuto', data3.getMinutes());// extrai os minutos
+console.log('Segundo', data3.getSeconds());// extrai os segundos
+console.log('Milisegundos', data3.getMilliseconds());//extrai os milisegundos
+console.log('Dia da semana', data3.getDay());//extrai o dia da semana
+console.log(Date.now());//mostra os milesemos de segundos que estão além da data de marco zero
+const data4 = new Date(1693943487775);//pega os milesemos e trasforma na data que eles representam
+console.log(data4);
